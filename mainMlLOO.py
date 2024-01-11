618813e51b0758a43670d42d5a77549c59c5abf7
Peich-Liu <lpcn572@163.com>
2024-01-11 17:51:52 +0100

    CSP

--- mainMlLOO.py
+++ mainMlLOO.py
@@ -46,27 +46,7 @@ for fileName in os.listdir(dataDir):
     csp = CSP(n_components=4, norm_trace=False)
     csp.fit(epochs.get_data(), dataLabel)
     csp_features = csp.transform(epochs.get_data())
-    # from mpl_toolkits.mplot3d import Axes3D  # 导入3D支持
 
-    # feature_1 = csp_features[:, 3]
-    # feature_2 = csp_features[:, 1]
-    # feature_3 = csp_features[:, 2]
-
-    # fig = plt.figure(figsize=(10, 8))
-    # ax = fig.add_subplot(111, projection='3d')
-
-    # ax.scatter(feature_1[dataLabel == 0], feature_2[dataLabel == 0], feature_3[dataLabel == 0], 
-    #         c='blue', label='Class 0', alpha=0.5)
-    # ax.scatter(feature_1[dataLabel == 1], feature_2[dataLabel == 1], feature_3[dataLabel == 1], 
-    #         c='red', label='Class 1', alpha=0.5)
-
-    # ax.set_xlabel('CSP Feature 1')
-    # ax.set_ylabel('CSP Feature 2')
-    # ax.set_zlabel('CSP Feature 3')
-    # ax.set_title('3D Plot of CSP Features')
-    # ax.legend()
-
-    # plt.show()
     feature_1 = csp_features[:, 1]
     feature_2 = csp_features[:, 2]
 
@@ -81,8 +61,8 @@ for fileName in os.listdir(dataDir):
 
     plt.show()
     csp.plot_patterns(epochs.info)
-    csp_component_1 = csp_features[:, 2]
-    csp_component_2 = csp_features[:, 3]
+    csp_component_1 = csp_features[:, 0]
+    csp_component_2 = csp_features[:, 1]
     plt.figure()
     plt.plot(csp_component_1, label='Label=0')
     plt.plot(csp_component_2, label='Label=1')
@@ -93,24 +73,6 @@ for fileName in os.listdir(dataDir):
     plt.ylabel('CSP Feature Value')
 
     plt.show()
-
-    # class_0_indices = dataLabel == 0
-    # class_1_indices = dataLabel == 1
-
-    # for i in range(2):  
-    #     feature_class_0 = csp_features[class_0_indices, i]
-    #     feature_class_1 = csp_features[class_1_indices, i]
-        
-    #     plt.figure(figsize=(8, 6))
-    #     plt.scatter(feature_class_0, [0] * len(feature_class_0), label='Label 0', alpha=0.5)
-    #     plt.scatter(feature_class_1, [1] * len(feature_class_1), label='Label 1', alpha=0.5)
-        
-    #     plt.title(f'CSP Component {i+1}')
-    #     plt.xlabel('Feature Value')
-    #     plt.yticks([0, 1], ['Label 0', 'Label 1'])
-    #     plt.legend()
-
-    #     plt.show()
 
     print("123")
 ########################################
